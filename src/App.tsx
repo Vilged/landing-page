eb686324393bd0169ea7c8067bc2c2329a606c2c
Eduard Vilgauk <eduard.vilgauk@gmail.com>
2026-04-15 20:51:20 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import "./App.css";
+import backgroundImage from "./assets/hintergrundBild.jpg";
+
+function App() {
+  return (
+    <main
+      className="landing-page"
+      style={{ backgroundImage: `url(${backgroundImage})` }}
+    >
+      <div className="overlay" />
+      <section className="content">
+      <h1>Eduard Vilgauk</h1>
+        <p className="role">Softwareentwickler</p>
+        <div className="links">
+          <a
+            href="https://www.linkedin.com/in/eduard-vilgauk-214a65212/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            LinkedIn
+          </a>
+          <a
+            href="https://github.com/dein-github"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
